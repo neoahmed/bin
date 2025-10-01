5c720ec0e1a02bdd04c77f06947f090b282b69a5
neoahmed <neoahmed@proton.me>
2025-10-01 09:47:54 +0300

    chore(exit) update kill dwm cmd

--- exit.sh
+++ exit.sh
@@ -3,5 +3,5 @@ choice=$(printf "Lock\nShutdown\nReboot\nExit" | dmenu -i -p "Choose action: ")
 [ "$choice" = "Lock"     ] && slock 2>/dev/null
 [ "$choice" = "Shutdown" ] && doas poweroff 2>/dev/null
 [ "$choice" = "Reboot"   ] && doas reboot 2>/dev/null
-[ "$choice" = "Exit"     ] && pkill dwm
+[ "$choice" = "Exit"     ] && kill $(pidof -s dwm)
 
